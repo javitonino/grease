3249710fefbe1c26f30f4dfb393ed33d676b0347
Javier Torres <jtorres@carto.com>
2016-12-16 16:53:09 +0100

    Add milestone to project view

--- github.com/kanban/projects.user.js
+++ github.com/kanban/projects.user.js
@@ -3,21 +3,22 @@
 // @namespace   github.javiertorres.eu
 // @include     https://github.com/orgs/*/projects/*
 // @include     https://github.com/*/*/projects/*
-// @version     1.0.0
+// @version     1.0.1
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @require     https://code.jquery.com/jquery-3.1.1.min.js
 // @updateURL   https://raw.githubusercontent.com/javitonino/grease/master/github.com/kanban/projects.user.js
 // ==/UserScript==
 
-var ISSUE_TAG_STYLE = 'class="_issue_tag" style="border: 1px solid rgba(0, 0, 0, 0.1); border-radius: 3px; font-size: 12px; padding: 2px 5px;"';
-var ISSUE_CACHE = {};
+var ISSUE_TAG_STYLE = 'class="_issue_tag" style="margin-right: 5px; border: 1px solid rgba(0, 0, 0, 0.1); border-radius: 3px; font-size: 12px; padding: 2px 5px;"';
+var ISSUE_REFERENCES_CACHE = {};
+var ISSUE_DATA_CACHE = {};
 var TOKEN = GM_getValue('oauth_token');
 var USER_LOGIN = $('meta[name=user-login]').attr('content');
 var IGNORED_COLUMNS = ['TODO', 'Done'];
 
-function getIssueData(card_link, callback) {
-  var cache = ISSUE_CACHE[card_link];
+function getIssueTimeline(card_link, callback) {
+  var cache = ISSUE_REFERENCES_CACHE[card_link];
   if (cache) { return callback(cache); }
 
   $.ajax('https://api.github.com/repos' + card_link + '/timeline', {
@@ -29,7 +30,23 @@ function getIssueData(card_link, callback) {
     },
     dataType: 'json'
   }).done(function(data) {
-    ISSUE_CACHE[card_link] = data;
+    ISSUE_REFERENCES_CACHE[card_link] = data;
+    callback(data);
+  });
+}
+
+
+function getIssueData(card_link, callback) {
+  var cache = ISSUE_DATA_CACHE[card_link];
+  if (cache) { return callback(cache); }
+
+  $.ajax('https://api.github.com/repos' + card_link, {
+    headers: {
+      Authorization: 'token ' + TOKEN
+    },
+    dataType: 'json'
+  }).done(function(data) {
+    ISSUE_DATA_CACHE[card_link] = data;
     callback(data);
   });
 }
@@ -70,15 +87,20 @@ function addPRLinks(card) {
   if (IGNORED_COLUMNS.includes(column)) { return; }
 
   var card_link = card.find('h5 a').attr('href');
-  getIssueData(card_link, function(data) {
+  getIssueTimeline(card_link, function(data) {
     data.forEach(function(i) {
       if (i.event == 'cross-referenced' && i.source.type === 'issue' && i.source.issue.pull_request) {
         var url = i.source.issue.html_url;
         var number = url.split('/').pop();
         url = url.replace('/repos', '').replace('api.', '');
-        card.append('<a href="' + url + '" ' + ISSUE_TAG_STYLE + '>#' + number + '</a>');
+        card.append('<a href="' + url + '" ' + ISSUE_TAG_STYLE + '><strong>PR</strong>#' + number + '</a>');
       }
     });
+  });
+  getIssueData(card_link, function(data) {
+    if (data.milestone) {
+      card.append('<a href="' + data.milestone.html_url + '" ' + ISSUE_TAG_STYLE + '><strong>MS</strong> ' + data.milestone.title + '</a>');
+    }
   });
 }
 
