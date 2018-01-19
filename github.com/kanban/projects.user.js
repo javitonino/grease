ee326bd015e553eda60fad6bbdaf1ca378504f1a
Javier Torres <jtorres@carto.com>
2018-01-19 14:19:14 +0100

    No more jQuery!

--- github.com/kanban/projects.user.js
+++ github.com/kanban/projects.user.js
@@ -3,23 +3,21 @@
 // @namespace   github.javitonino.eu
 // @include     https://github.com/orgs/*/projects/*
 // @include     https://github.com/*/*/projects/*
-// @version     1.0.17
+// @version     1.0.18
 // @require  https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @grant    GM.getValue
 // @grant    GM_getValue
 // @grant    GM.setValue
 // @grant    GM_setValue
-// @require     https://code.jquery.com/jquery-3.1.1.min.js
 // @updateURL   https://raw.githubusercontent.com/javitonino/grease/master/github.com/kanban/projects.user.js
 // ==/UserScript==
 
 var MILESTONE_CACHE = {};
 var ISSUE_DATA_CACHE = {};
-var USER_LOGIN = $('meta[name=user-login]').attr('content');
+var USER_LOGIN = document.querySelector('meta[name=user-login]').getAttribute('content');
 var IGNORED_COLUMNS = ['Done', 'Done in previous shifts'];
 var REVIEWER_BLACKLIST = ['houndci-bot'];
 var TOKEN = null;
-var PRIORITY_STYLE = 'background: repeating-linear-gradient(-45deg, #fff, #fff 20px, #fee 20px, #fee 21px, #fff 22px); border-color: #faa !important;';
 var REVIEWS = {
   'PENDING' : { 'color': '#fbca04', text: '?' },
   'APPROVED' : { 'color': '#0e8a16', text: '✔' },
@@ -29,6 +27,13 @@ var REVIEWS = {
   'OTHERS' : { 'color': '#cccccc', text: 'O' }
 };
 
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim();
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+
 function getIssueData(card_link, callback) {
   if (card_link === void 0) {
     return;
@@ -36,65 +41,69 @@ function getIssueData(card_link, callback) {
 
   var cache = ISSUE_DATA_CACHE[card_link];
   if (cache) { return callback(cache); }
-  
+
   var link_parts = card_link.split('/');
   var query = 'query { repository(owner: "' + link_parts[1] + '", name: "' + link_parts[2] + '") { issue(number: ' + link_parts[4] + ') {  milestone { resourcePath } timeline(first: 100) {  nodes { ... on CrossReferencedEvent {source { ... on PullRequest { number, resourcePath, reviews(last: 1) {nodes { state, author { login } } } } } } } } } } }';
 
-  $.ajax('https://api.github.com/graphql', {
+  fetch(new Request('https://api.github.com/graphql', {
     headers: {
       Authorization: 'token ' + TOKEN
     },
-    dataType: 'json',
-    data: JSON.stringify({ "query": query }),
+    body: JSON.stringify({ "query": query }),
     method: 'POST'
-  }).done(function(data) {
-    ISSUE_DATA_CACHE[card_link] = data;
-    callback(data);
+  })).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        ISSUE_DATA_CACHE[card_link] = data;
+        callback(data);
+      });
+    }
   });
 }
 
 
-function getMilestoneData(milestone_link, callback) { 
+function getMilestoneData(milestone_link, callback) {
   if (milestone_link === void 0) {
     return;
   }
 
   var cache = MILESTONE_CACHE[milestone_link];
   if (cache) { return callback(cache); }
-  
-  $.ajax('https://api.github.com/repos' + milestone_link, {
+
+  fetch(new Request('https://api.github.com/repos' + milestone_link, {
     headers: {
       Authorization: 'token ' + TOKEN
-    },
-    dataType: 'json'
-  }).done(function(data) {
+    }
+  })).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
     MILESTONE_CACHE[milestone_link] = data;
     callback(data);
+      });
+    }
   });
 }
 
 
 function assignIssue(card_link) {
-  $.ajax('https://api.github.com/repos' + card_link + '/assignees', {
+  fetch(new Request('https://api.github.com/repos' + card_link + '/assignees', {
     method: 'POST',
     headers: {
       Authorization: 'token ' + TOKEN
     },
-    dataType: 'json',
-    data: JSON.stringify({ assignees: [USER_LOGIN] })
-  });
+    body: JSON.stringify({ assignees: [USER_LOGIN] })
+  }));
 }
 
 
 function unassignIssue(card_link) {
-  $.ajax('https://api.github.com/repos' + card_link + '/assignees', {
+  fetch(new Request('https://api.github.com/repos' + card_link + '/assignees', {
     method: 'DELETE',
     headers: {
       Authorization: 'token ' + TOKEN
     },
-    dataType: 'json',
-    data: JSON.stringify({ assignees: [USER_LOGIN] })
-  });
+    body: JSON.stringify({ assignees: [USER_LOGIN] })
+  }));
 }
 
 
@@ -104,7 +113,7 @@ function buildReviewSpan (review) {
   style += 'padding-left: 2px;';
   style += 'color: ' + REVIEWS[review].color + ';';
 
-  return $('<span style="' + style + '">' + REVIEWS[review].text + '</span>');
+  return htmlToElement('<span style="' + style + '">' + REVIEWS[review].text + '</span>');
 }
 
 
@@ -115,7 +124,7 @@ function getPRInfoFromUrl (url) {
     return {
       owner: match[1],
       repo: match[2],
-      id: match[3] 
+      id: match[3]
     };
   }
   return null;
@@ -151,24 +160,21 @@ function parseReviews (data, pr) {
 
 
 function addPRLinks(card) {
-  if (card.data('links-loaded')) {
-      return;
+  if (card.getAttribute('links-loaded')) {
+    return;
   } else {
-      card.data('links-loaded', true);
-  }
-  
-  if (card.find('.issue-card-label:contains(major-priority)').length > 0) {
-    card.prop('style', PRIORITY_STYLE);
+    card.setAttribute('links-loaded', true);
   }
 
-  var column = card.closest('.project-column').find('.js-project-column-name').html();
+  if (!card.closest('.project-column')) { return; }
+  var column = card.closest('.project-column').querySelector('.js-project-column-name').innerHTML;
   if (IGNORED_COLUMNS.includes(column)) { return; }
 
-  var card_link = card.find('a.h5').attr('href');
-  
+  var card_link = card.querySelector('a.h5') && card.querySelector('a.h5').getAttribute('href');
+
   if (card_link) {
     // Issue
-    card.append('<div class="milestone-container pl-5 p-2"></div>');
+    card.append(htmlToElement('<div class="milestone-container pl-5 p-2"></div>'));
 
     getIssueData(card_link, function(data) {
       if (!data.data.repository.issue) {
@@ -178,12 +184,12 @@ function addPRLinks(card) {
       data.data.repository.issue.timeline.nodes.forEach(function(i) {
         if (i.source && i.source.number) {
           var url = i.source.resourcePath;
-          var labels = card.find('.labels');
-          if (labels.length == 0) {
-            labels = $('<span class="labels d-block pb-1 pr-6"></span>');
-            card.find('.d-block').after(labels);
+          var labels = card.querySelector('.labels');
+          if (!labels) {
+            labels = htmlToElement('<span class="labels d-block pb-1 pr-6"></span>');
+            if (card.querySelector('.d-block.pr-6')) { card.querySelector('.d-block.pr-6').after(labels); }
           }
-          var pr = $('<a class="issue-card-label css-truncate css-truncate-target label mt-1 v-align-middle labelstyle-fbca04 linked-labelstyle-fbca04 tooltipped tooltipped-n" href="' + i.source.resourcePath + '" style="color: #4078c0; border: 1px solid #DDD; border-radius: 3px; box-shadow: none; margin-right: 3px;">#' + i.source.number + '</a>');
+          var pr = htmlToElement('<a class="issue-card-label css-truncate css-truncate-target label mt-1 v-align-middle labelstyle-fbca04 linked-labelstyle-fbca04 tooltipped tooltipped-n" href="' + i.source.resourcePath + '" style="color: #4078c0; border: 1px solid #DDD; border-radius: 3px; box-shadow: none; margin-right: 3px;">#' + i.source.number + '</a>');
           labels.append(pr);
 
           parseReviews(i.source.reviews.nodes, pr);
@@ -195,73 +201,87 @@ function addPRLinks(card) {
           var total_issues = data.open_issues + data.closed_issues;
           var percent = data.closed_issues / total_issues * 100;
           var progress_bar = ' background: linear-gradient(90deg, #6cc644 ' + percent + '%, #EEE ' + percent +'%)';
-          card.find('.milestone-container').append('<div style="height: 2px; margin: 0 0 11px 0; ' + progress_bar + '"></div><a class="text-gray" style="font-size: 12px; line-height: 14px; display: block;" href="' + data.html_url + '">' + data.title + ' (' + Math.round(percent) + '%)</a>');
+          card.querySelector('.milestone-container').append(htmlToElement('<div style="height: 2px; margin: 0 0 11px 0; ' + progress_bar + '"></div><a class="text-gray" style="font-size: 12px; line-height: 14px; display: block;" href="' + data.html_url + '">' + data.title + ' (' + Math.round(percent) + '%)</a>'));
         });
       }
     });
   } else {
     // Note
-    var text = card.find('.mr-4 p').html();
+    var title = card.querySelector('.mr-4 p');
+    var text = title.innerHTML;
     if (text.startsWith('==') && text.endsWith('==')) {
-      card.find('.mr-4 p').html(text.replace(/=/g, ''));
-      card.find('.mr-4 small').remove();
-      card.find('.mr-4 p').css('margin-bottom', 0);
-      card.find('.mr-4 p').css('font-weight', 'bold');
-      card.css('background-color', 'rgb(225, 228, 232)');
+      title.innerHTML = text.replace(/=/g, '');
+      card.querySelector('small').remove();
+      title.setAttribute('style', 'margin-bottom: 0; font-weight: bold;');
+      card.setAttribute('style', 'background-color: rgb(225, 228, 232) !important;');
     }
   }
 }
 
 
 function addMenuOptions(card) {
-  if (card.data('menu-loaded')) {
+  if (card.getAttribute('menu-loaded')) {
     return;
   } else {
-    card.data('menu-loaded', true);
+    card.setAttribute('menu-loaded', true);
   }
 
-  var card_link = card.find('a.h5').attr('href');
-  var assignees = card.find('.avatar').map(function(i, v) { return $(v).data('assignee'); }).toArray();
+  if (!card.querySelector('a.h5') || !card.querySelector('.dropdown-menu')) { return; }
+
+  var card_link = card.querySelector('a.h5').getAttribute('href');
+  var avatars = card.querySelectorAll('.avatar');
+
+  var assignees = [];
+  for (var i = 0; i < avatars.length; i++) {
+    assignees.push(avatars[i].getAttribute('data-assignee'));
+  }
 
   var assign_button;
   if (assignees.includes(USER_LOGIN)) {
-    assign_button = $('<button class="dropdown-item text-left btn-link js-menu-close">Unassign myself</button>');
-    assign_button.on('click', function() {
+    assign_button = htmlToElement('<button class="dropdown-item text-left btn-link js-menu-close">Unassign myself</button>');
+    assign_button.addEventListener('click', function() {
       unassignIssue(card_link);
       assign_button.remove();
     });
   } else {
-    assign_button = $('<button class="dropdown-item text-left btn-link js-menu-close">Assign myself</button>');
-    assign_button.on('click', function() {
+    assign_button = htmlToElement('<button class="dropdown-item text-left btn-link js-menu-close">Assign myself</button>');
+    assign_button.addEventListener('click', function() {
       assignIssue(card_link);
       assign_button.remove();
     });
   }
-  card.find('.dropdown-menu').prepend(assign_button);
+  card.querySelector('.dropdown-menu').prepend(assign_button);
 }
 
 (async function() {
 	TOKEN = await GM.getValue('oauth_token');
-  $(function() {
-    if (!TOKEN) {
-      TOKEN = prompt("I need an Oauth token with the repo scope from https://github.com/settings/tokens", "");
-      if (TOKEN) {
-        GM.setValue('oauth_token', TOKEN);
-      } else {
-        return;
+  if (!TOKEN) {
+    TOKEN = prompt("I need an Oauth token with the repo scope from https://github.com/settings/tokens", "");
+    if (TOKEN) {
+      GM.setValue('oauth_token', TOKEN);
+    } else {
+      return;
+    }
+  }
+
+  setTimeout(function() {
+    var observer = new MutationObserver(function(mutationsList) {
+      for(var mutation of mutationsList) {
+        var card = mutation.target;
+        addPRLinks(card);
+        addMenuOptions(card);
       }
+    });
+    var columns = document.querySelectorAll('.js-project-column-cards');
+    for (var i = 0; i < columns.length; i++) {
+      observer.observe(columns[i], { childList: true, attributes: false, characterData: false });
     }
 
-    $(document).on('DOMSubtreeModified', '.issue-card', function(e) {
-      var card = $(e.currentTarget);
+    var issues = document.querySelectorAll('.issue-card');
+    for (var i = 0; i < issues.length; i++) {
+      var card = issues[i];
       addPRLinks(card);
       addMenuOptions(card);
-    });
-
-    $('.issue-card').each(function(i, v) {
-      var card = $(v);
-      addPRLinks(card);
-      addMenuOptions(card);
-    });
-  });
+    }
+  }, 1000);
 })();
